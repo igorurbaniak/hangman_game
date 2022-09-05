16fde7908ab3f5a853352aefe02455e601f1de98
igorurbaniak <igorurbaniakq@wp.pl>
2022-09-05 23:11:14 +0200

    Add points, hangman images and player lifes

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -10,37 +10,74 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
-  TextStyle gamestyle = GoogleFonts.audiowide(
-    fontSize: 25,
-    color: Colors.white,
-    fontWeight: FontWeight.w500,
-  );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black45,
+      backgroundColor: Colors.black26,
       appBar: NewGradientAppBar(
         gradient: const LinearGradient(
-          colors: [
-            Colors.cyan,
-            Colors.red,
-          ],
+          colors: [Colors.cyan, Colors.red],
         ),
         centerTitle: true,
         title: Text(
           'Hangman',
-          style: gamestyle,
+          style: GoogleFonts.audiowide(
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
         ),
         actions: [
           IconButton(
             iconSize: 35,
             onPressed: () {},
-            icon: const Icon(
-              Icons.volume_up_sharp,
-            ),
+            icon: const Icon(Icons.volume_up_sharp),
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(color: Colors.lightBlue),
+                width: MediaQuery.of(context).size.width / 3.5,
+                height: 30,
+                child: Center(
+                  child: Text(
+                    '25 points',
+                    style: GoogleFonts.audiowide(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Image(
+                image: AssetImage('images/hangman10.png'),
+                width: 155,
+                height: 155,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                '7 lives left',
+                style: GoogleFonts.audiowide(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
